fa858734f7e41d4597dc69cb576c394fcc90f065
vinayakentc <vinayakchavanentc@gmail.com>
2021-02-10 18:11:27 +0530

    start fresh by creating a tic tac toe board

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -1,5 +1,20 @@
 package tictactoe;
 
-public class TicTacToe {
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class TicTacToe {
+	static String[] board;
+
+	public static void main(String[] args) {
+		board = new String[9];
+		populateEmptyBoard();
+	}
+
+	static void populateEmptyBoard() {
+		for (int a = 0; a < 9; a++) {
+			board[a] = String.valueOf(a+1);
+		}
+	}
 }
