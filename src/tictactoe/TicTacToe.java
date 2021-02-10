25065c2213714ec65173ae69cf618e01944154b2
vinayakentc <vinayakchavanentc@gmail.com>
2021-02-10 19:50:35 +0530

    make a move desired location in the board

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -35,7 +35,7 @@ public class TicTacToe {
 		return turn;
 	}
 	
-	//This method displays the board 
+	// method to displays the board 
 		public static void printBoard() {
 			System.out.println("/---|---|---\\");
 			System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
@@ -44,5 +44,60 @@ public class TicTacToe {
 			System.out.println("|-----------|");
 			System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
 			System.out.println("/---|---|---\\");
+		}
+		
+		public static String checkWinner() {
+			for (int a = 0; a < 8; a++)
+			{
+				String line = null;
+				switch (a)
+				{
+					case 0:
+						line = board[0] + board[1] + board[2];
+						break;
+					case 1:
+						line = board[3] + board[4] + board[5];
+						break;
+					case 2:
+						line = board[6] + board[7] + board[8];
+						break;
+					case 3:
+						line = board[0] + board[3] + board[6];
+						break;
+					case 4:
+						line = board[1] + board[4] + board[7];
+						break;
+					case 5:
+						line = board[2] + board[5] + board[8];
+						break;
+					case 6:
+						line = board[0] + board[4] + board[8];
+						break;
+					case 7:
+						line = board[2] + board[4] + board[6];
+						break;
+				}
+				if (line.equals("XXX"))
+				{
+					return "X";
+				}
+				else if (line.equals("OOO"))
+				{
+					return "O";
+				}
+			}
+
+			for (int a = 0; a < 9; a++)
+			{
+				if (Arrays.asList(board).contains(String.valueOf(a+1)))
+				{
+					break;
+				}
+				else if (a == 8)
+					return "draw";
+			}
+
+			System.out.println(turn + "'s turn; enter a slot number to place " + turn + " in:");
+			return null;
 		}
 }
